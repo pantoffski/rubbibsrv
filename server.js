c68047b5746be2bd20c495b7626b775283d9688f
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2017-12-03 14:51:36 +0700

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen((process.env.PORT || 5000), function () {
 });
 app.all('/', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.get('/runners/:tStamp', function (req, res) {
